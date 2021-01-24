115e8dedde2346611dfee40ee27a41bec0eb13ed
Fiona Spuler <56720149+fiona511@users.noreply.github.com>
2021-01-24 15:05:20 +0000

    another bit

--- 1_create_results.R
+++ 1_create_results.R
@@ -13,13 +13,13 @@ setwd(github_folder)
 
 ### download adta from ECB website
 
-source(get_data.R)
+source("0_get_data.R")
 
 ###  transform data into PACTA readable format
 
 data <- raw_data %>%
   select(TYPE, HAIRCUT_CATEGORY, ISIN_CODE) %>%
-  mutate(NumberofShares=NA, MarketValue=100, Currency="EUR")
+  mutate(TYPE="ECB", NumberofShares=NA, MarketValue=100, Currency="EUR")
 
 colnames(data)<-c("Investor.Name", "Portfolio.Name", "ISIN", "NumberofShares", "MarketValue", "Currency")
 write_excel_csv(data, "/Users/fionaspuler/Dropbox (2° Investing)/PortCheck_v2/10_Projects/ecb_collateral/20_Raw_Inputs/ecb_collateral_Input.csv")
@@ -27,9 +27,22 @@ write_excel_csv(data, "/Users/fionaspuler/Dropbox (2° Investing)/PortCheck_v2/1
 ### run PACTA
 
 source(paste0(pacta_code_folder, "/1_portfolio_check_initialisation.R"))
-source(paste0(pacta_code_folder, "2_project_input_analysis.R"))
+pacta_code_folder<-"/Users/fionaspuler/Documents/GitHub/PACTA_analysis_ecb"
+source(paste0(pacta_code_folder, "/2_project_input_analysis.R"))
 source(paste0(pacta_code_folder, "/3_run_analysis.R"))
 
+### re-load packages
+
+rm(list=ls())
+library(tidyverse)
+library(httr)
+library(r2dii.utils)
+library(treemap)
+
+github_folder<-"/Users/fionaspuler/Documents/GitHub/Collateral-Assets-Dashboard"
+pacta_code_folder<-"/Users/fionaspuler/Documents/GitHub/PACTA_analysis_ecb"
+pacta_results_folder<-"/Users/fionaspuler/Dropbox (2° Investing)/PortCheck_v2/10_Projects/ecb_collateral"
+ald_folder<-"/Users/fionaspuler/Dropbox (2° Investing)/PortCheck/00_Data/07_AnalysisInputs/2019Q4_10042020_2020"
 
 
 ### visualize results
@@ -37,7 +50,20 @@ source(paste0(pacta_code_folder, "/3_run_analysis.R"))
 setwd(github_folder)
 
 source("0_visualisation_functions.R")
+
 startyear<-2020
+
+duplicated_companies <- c("Petrol Dd Ljubljana", 	
+                          "Bmw Intl Investment Bv", "Bmw Us Capital Llc", "Crh Finland Services Oyj",
+                          "Crh Funding Bv", "Crh Smw Finance Dac", "Daimler Canada Finance", "Daimler International Finance",
+                          "E.On Intl Finance Bv", "Enel Finance Intl Nv", "Eni Finance International Sa",
+                          "Ge Capital Euro Funding", "Ge Capital Uk Funding Unlimited Co", "Heidelbergcement Fin Lux",
+                          "Holcim Us Finance Sarl &" , 	
+                          "Iberdrola Fin Ireland", "Iberdrola Finanzas Sau", 	
+                          "Mercedes-Benz Fin Serv I", "Total Capital Intl Sa", "Toyota Motor Finance Bv",
+                          "Volkswagen Financial Services Nv", "Volkswagen International Finan", "Volkswagen Leasing Gmbh")
+
+
 
 # load the PACTA results
 
@@ -57,22 +83,32 @@ audit<-total<-readRDS(paste0(pacta_results_folder, "/30_Processed_Inputs/ecb_col
 
 market_bonds<-readRDS(paste0(ald_folder, "/0_MarketPortfolios_bonds_portfolio.rda"))
 
+
+
 # overview of companies in PACTA sectors
 
-overview_companies <- total %>%
-  select(company_name, financial_sector, ) %>%
+overview_companies <- bonds %>%
+  filter(!(scenario %in% c("GECO2019_1.5c", "GECO2019_2c_m", "GECO2019_ref"))) %>%
+  filter(year==startyear, scenario_geography=="Global") %>%
+  select(company_name, financial_sector, plan_sec_prod) %>%
   distinct() %>%
+  filter(!(company_name %in% duplicated_companies)) %>%
   filter(financial_sector!="Other", financial_sector!="Unclassifiable") %>%
-  mutate(count=1) %>%
-  select(financial_sector, company_name, count)
+  group_by(financial_sector) %>%
+  mutate(total_production=sum(plan_sec_prod, na.rm=T)) %>%
+  mutate(number=length(unique(company_name))) %>%
+  ungroup() %>%
+  mutate(production_share = plan_sec_prod*number/total_production) %>%
+  select(financial_sector, company_name, production_share)
   
-png(filename="plots/tree.png",width=800, height=800)
-treemap_plot <- treemap(overview_companies, index=c("financial_sector","company_name"),     vSize="count", 
+png(filename="plots/tree_new.png",width=800, height=800)
+treemap_plot <- treemap(overview_companies, index=c("financial_sector","company_name"),     vSize="production_share", 
                         type="index",
                         palette=c(primary_blue, secondary_red , primary_grey, oil, hybrid_orange, secondary_moss_green),
                         title="Overview of companies in PACTA sectors in the collateral framework",
                         
-                        fontsize.labels=c(18,9),                # size of labels. Give the size per level of aggregation: size for group, size for subgroup, sub-subgroups...
+                        fontsize.labels=c(20,10),                # size of labels. Give the size per level of aggregation: size for group, size for subgroup, sub-subgroups...
+                        fontsize.title=24,
                         fontcolor.labels=c("white","white"),    # Color of labels
                         fontface.labels=c(2,1),                  # Font of labels: 1,2,3,4 for normal, bold, italic, bold-italic...
                         bg.labels=c("transparent"),              # Background color of labels
@@ -86,13 +122,14 @@ treemap_plot <- treemap(overview_companies, index=c("financial_sector","company_
 )
 dev.off()
 
+
 # overview of type of financial instrument
 
 security_type <- as.data.frame(table(total$security_type))
 
 
 
-# share of renewable  and coal power capacity
+# companies' share of renewable  and coal power capacity
 
 p1<-plot_tech_share("RenewablesCap", "Share of renewable power capacity as percent of total capacity")
 p1
@@ -123,7 +160,7 @@ ggsave(filename="plots/map_hydrocap.png", width=30, height=15, units="cm", dpi=3
 
 
 
-# plot: coal, oil coal power, renewables aggregate fiveyear expansion plans
+# aggregate fiveyear expansion plans
 
 fiveyear_oil <- fiveyeargrowth(tech_choice="Oil", geography="Global", plot_title="Oil Production", ytitle="Million Barrels Oil (mbbl)")
 ggsave(filename="plots/fiveyear_oil.png", width=20, height=20, units="cm", dpi=350)
@@ -156,6 +193,35 @@ ggsave(filename="plots/fiveyear_hybrid.png", width=20, height=20, units="cm", dp
 fiveyear_ren <- fiveyeargrowth(tech_choice="RenewablesCap", geography="GlobalAggregate", plot_title="Power Capacity from Renewable Energy", ytitle="Capacity (MW)")
 fiveyear_ren
 ggsave(filename="plots/fiveyear_ren.png", width=20, height=20, units="cm", dpi=350)
+
+
+
+# Technology mix in auto and power sector compared to market
+
+auto_techmix <- plot_tech_share_sector(technologies=c("ICE", "Hybrid", "Electric"), "Automotive", palette=auto_palette)
+auto_techmix
+ggsave(filename="plots/techmix_auto.png", width=30, height=15, units="cm", dpi=350)
+
+power_techmix <- plot_tech_share_sector(technologies=c("CoalCap", "OilCap", "GasCap", "NuclearCap", "HydroCap", "RenewablesCap"), "Power", palette=power_palette)
+power_techmix
+ggsave(filename="plots/techmix_power.png", width=30, height=15, units="cm", dpi=350)
+
+
+# Technology mix by haircut type
+
+bonds2<-readRDS(paste0(pacta_results_folder, "/40_Results/Bonds_results_company.rda")) %>%
+  filter(portfolio_name!="Meta Portfolio")
+
+haircut_techmix <- bonds2 %>%
+  filter(!(scenario %in% c("GECO2019_1.5c", "GECO2019_2c_m", "GECO2019_ref"))) %>%
+  filter(scenario_geography=="Global") %>%
+  filter(year==startyear) %>%
+  select(portfolio_name, technology, plan_tech_share) %>%
+  distinct() %>%
+  filter(technology %in% technologies) %>%
+  group_by(portfolio_name) %>%
+  mutate(plan_tech_share=plan_tech_share/sum(plan_tech_share, na.rm=T)) %>%
+  ungroup()
 
 
 
